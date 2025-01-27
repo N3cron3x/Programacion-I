b860d54a8f28fc96f1fd3fe7cf141f9e1d7a10b5
N3cron3x <munozcastilloluisalejandro@gmail.com>
2025-01-27 15:52:10 -0400

    Editado Final

--- Ejercicio con Funciones/Comprobador de Años Bisiestos.cs
+++ Ejercicio con Funciones/Comprobador de Años Bisiestos.cs
@@ -6,21 +6,21 @@
         {
             Console.Write("Por favor, introduce un año: ");
             string? entrada = Console.ReadLine();
-            int añoIngresado;
+            int añoIng;
 
-            while (!int.TryParse(entrada, out añoIngresado) || añoIngresado < 1582)
+            while (!int.TryParse(entrada, out añoIng) || añoIng < 1582)
             {
                 Console.WriteLine("El año debe ser un número igual o mayor a 1582. Intenta nuevamente.");
                 Console.Write("Introduce un año válido: ");
                 entrada = Console.ReadLine();
             }
 
-            string resultado = EsAñoBisiesto(añoIngresado) 
-                ? $"El año {añoIngresado} es un año bisiesto." 
-                : $"El año {añoIngresado} no es un año bisiesto.";
+            string resultado = EsAñoBis(añoIng) 
+                ? $"El año {añoIng} es un año bisiesto." 
+                : $"El año {añoIng} no es un año bisiesto.";
             Console.WriteLine(resultado);
 
-            static bool EsAñoBisiesto(int año)
+            static bool EsAñoBis(int año)
             {
                 if (año % 400 == 0) return true;
                 if (año % 100 == 0) return false;
