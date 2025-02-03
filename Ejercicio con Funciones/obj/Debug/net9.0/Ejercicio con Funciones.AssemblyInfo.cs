78ea2df5b0e6ccbe5a8a4a616cb3d37bedead510
N3cron3x <munozcastilloluisalejandro@gmail.com>
2025-02-03 04:00:07 -0400

    Ejercicio hecho

--- Ejercicio con Funciones/obj/Debug/net9.0/Ejercicio con Funciones.AssemblyInfo.cs
+++ Ejercicio con Funciones/obj/Debug/net9.0/Ejercicio con Funciones.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ejercicio con Funciones")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85e5d254699c9ef26fa5908205e8f193cc6da94b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1be70a33e0a6176e3bce6d54a850981f26e96f08")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ejercicio con Funciones")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ejercicio con Funciones")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Patrones de Diseño/Patrones de Diseño.cs
+++ Patrones de Diseño/Patrones de Diseño.cs
@@ -1,1 +1,128 @@
-﻿
+﻿// Patrón de Diseño Creacional Prototype. Permite clonar objetos en lugar de crear nuevas instancias desde cero.
+
+public abstract class Prototype
+{
+    public abstract Prototype Clone();
+}
+
+public class ConcretePrototype : Prototype
+{
+    public string Data { get; set; }
+    
+    public ConcretePrototype(string data)
+    {
+        Data = data;
+    }
+    
+    public override Prototype Clone()
+    {
+        return new ConcretePrototype(Data);
+    }
+}
+
+// Patrón de Diseño Estructural Decorator. Agrega funcionalidad extra a un objeto sin modificar su estructura.
+
+public interface IComponent
+{
+    void Operation();
+}
+
+public class ConcreteComponent : IComponent
+{
+    public void Operation()
+    {
+        Console.WriteLine("Operación del componente base");
+    }
+}
+
+public abstract class Decorator : IComponent
+{
+    protected IComponent component;
+    
+    public Decorator(IComponent component)
+    {
+        this.component = component;
+    }
+    
+    public virtual void Operation()
+    {
+        component.Operation();
+    }
+}
+
+public class ConcreteDecorator : Decorator
+{
+    public ConcreteDecorator(IComponent component) : base(component) { }
+    
+    public override void Operation()
+    {
+        base.Operation();
+        Console.WriteLine("Operación adicional del decorador");
+    }
+}
+
+// Patrón de Diseño de Comportamiento Strategy. Permite intercambiar algoritmos de manera dinámica sin afectar el código cliente.
+
+public interface IStrategy
+{
+    void Execute();
+}
+
+public class ConcreteStrategyA : IStrategy
+{
+    public void Execute()
+    {
+        Console.WriteLine("Estrategia A ejecutada");
+    }
+}
+
+public class ConcreteStrategyB : IStrategy
+{
+    public void Execute()
+    {
+        Console.WriteLine("Estrategia B ejecutada");
+    }
+}
+
+public class Context
+{
+    private IStrategy strategy;
+    
+    public void SetStrategy(IStrategy strategy)
+    {
+        this.strategy = strategy;
+    }
+    
+    public void ExecuteStrategy()
+    {
+        strategy.Execute();
+    }
+}
+
+// Implementación en la función Main para probar los patrones de diseño. */
+
+class Program
+{
+    static void Main()
+    {
+        // Prototype
+        Console.WriteLine("--- Prototype ---");
+        ConcretePrototype prototype = new ConcretePrototype("Dato original");
+        ConcretePrototype clone = (ConcretePrototype)prototype.Clone();
+        Console.WriteLine("Clon: " + clone.Data);
+        
+        // Decorator
+        Console.WriteLine("--- Decorator ---");
+        IComponent component = new ConcreteComponent();
+        IComponent decoratedComponent = new ConcreteDecorator(component);
+        decoratedComponent.Operation();
+        
+        // Strategy
+        Console.WriteLine("--- Strategy ---");
+        Context context = new Context();
+        context.SetStrategy(new ConcreteStrategyA());
+        context.ExecuteStrategy();
+        context.SetStrategy(new ConcreteStrategyB());
+        context.ExecuteStrategy();
+    }
+}
