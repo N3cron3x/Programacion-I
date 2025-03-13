1606761a7fea6c3f0dd717a07bef2adba7a200a3
N3cron3x <munozcastilloluisalejandro@gmail.com>
2025-03-12 23:19:30 -0400

    Toques finales

--- Patrones de Diseño/obj/Debug/net9.0/Patrones de Diseño.AssemblyInfo.cs
+++ Patrones de Diseño/obj/Debug/net9.0/Patrones de Diseño.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Patrones de Diseño")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5b641aa075d49f5800dc144bc812b33f47bc0cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b2ee3c86bbf5f711004c0dfc21b14f3c26ef131")]
 [assembly: System.Reflection.AssemblyProductAttribute("Patrones de Diseño")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Patrones de Diseño")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
