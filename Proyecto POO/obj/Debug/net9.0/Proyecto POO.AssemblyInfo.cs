0b2ee3c86bbf5f711004c0dfc21b14f3c26ef131
N3cron3x <munozcastilloluisalejandro@gmail.com>
2025-03-12 22:34:30 -0400

    Base del nuevo Proyecto

--- Proyecto POO/obj/Debug/net9.0/Proyecto POO.AssemblyInfo.cs
+++ Proyecto POO/obj/Debug/net9.0/Proyecto POO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto POO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+938b5e8d79dbdeb44ce9cb8462a1fb757570b24a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5b641aa075d49f5800dc144bc812b33f47bc0cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto POO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto POO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
